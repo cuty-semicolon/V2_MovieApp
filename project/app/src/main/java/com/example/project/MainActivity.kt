5458d1e310422f95e5422074265931b2156ce615
leeyongjin <dragon02tv@gmail.com>
2021-08-04 21:00:00 +0900

    ⚡:: #11 MainActivity에 Navigation Drawer 구현 끝

--- project/app/src/main/java/com/example/project/MainActivity.kt
+++ project/app/src/main/java/com/example/project/MainActivity.kt
@@ -5,17 +5,18 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var toggle : ActionBarDrawerToggle
-
+    lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+        drawerLayout = findViewById(R.id.drawerLayout)
         val navView : NavigationView = findViewById(R.id.nav_view)
 
         toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
@@ -25,19 +26,32 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         navView.setNavigationItemSelectedListener {
+
+            it.isChecked = true
+
             when(it.itemId){
 
-                R.id.nav_home -> println("home")
-                R.id.nav_credit -> println("home")
-                R.id.nav_sports -> println("home")
-                R.id.nav_romance -> println("home")
-                R.id.nav_horror -> println("home")
-                R.id.nav_action -> println("home")
+                R.id.nav_home -> replaceFragment(HomeFragment(),it.title.toString())
+                R.id.nav_credit -> replaceFragment(CreditFragment(),it.title.toString())
+                R.id.nav_sports -> replaceFragment(SportsFragment(),it.title.toString())
+                R.id.nav_romance -> replaceFragment(RomanceFragment(),it.title.toString())
+                R.id.nav_horror -> replaceFragment(HorrorFragment(),it.title.toString())
+                R.id.nav_action -> replaceFragment(ActionFragment(),it.title.toString())
             }
 
             true
 
         }
+    }
+
+    private fun replaceFragment(fragment: Fragment,title : String){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout,fragment)
+        fragmentTransaction.commit()
+        drawerLayout.closeDrawers()
+        setTitle(title)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
